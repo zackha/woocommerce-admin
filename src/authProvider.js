6105ed265caacd964adfb3f6e0ee5837ca58ad40
Zack Hatlen <zckhtln@icloud.com>
2022-09-18 22:48:02 +0300

    Update authProvider.js

--- src/authProvider.js
+++ src/authProvider.js
@@ -1,39 +1,32 @@
 const authProvider = {
-
   login: ({ username, password }) => {
-    if (username !== 'demo' || password !== 'demo') {
-      return Promise.reject();
-    }
-    localStorage.setItem('username', username);
-    return Promise.resolve();
+      if (username !== 'john' || password !== '123') {
+          return Promise.reject();
+      }
+      localStorage.setItem('username', username);
+      return Promise.resolve();
   },
-
   logout: () => {
-    localStorage.removeItem('username');
-    return Promise.resolve();
-  },
-
-  checkAuth: () =>
-    localStorage.getItem('username') ? Promise.resolve() : Promise.reject(),
-
-  checkError:  (error) => {
-    const status = error.status;
-    if (status === 401 || status === 403) {
       localStorage.removeItem('username');
-      return Promise.reject();
-    }
-    return Promise.resolve();
+      return Promise.resolve();
   },
-
+      checkAuth: () =>
+      localStorage.getItem('username') ? Promise.resolve() : Promise.reject(),
+  checkError:  (error) => {
+      const status = error.status;
+      if (status === 401 || status === 403) {
+          localStorage.removeItem('username');
+          return Promise.reject();
+      }
+      // other error code (404, 500, etc): no need to log out
+      return Promise.resolve();
+  },
   getIdentity: () =>
-    Promise.resolve({
-      id: 'user',
-      fullName: 'Brooks Hatlen',
-      avatar: '',
-    }),
-
+      Promise.resolve({
+          id: 'user',
+          fullName: 'John Doe',
+      }),
   getPermissions: () => Promise.resolve(''),
-  
 };
 
 export default authProvider;
